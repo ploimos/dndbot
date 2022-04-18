b67f64f595309d077f6e8f632252a5959aa929c4
ploimos <sinnoh98@gmail.com>
2022-04-18 21:47:19 +0200

    sda

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const client = new Discord.Client(
 var ruolo = "965547318009016330" //id ruolo admin
 var canale = "965263672421277748" //id canale dove scrive il bot
 var server = "965263672421277746" //id server
-const oldRoles = oldMember.roles.cache;
-const newRoles = newMember.roles.cache;
 
 //client.login(process.env.token)
 client.login("OTY1MjYyOTEwNTc2Mjk1OTM2.YlwpIw.3g4joeLLpp_ykDY08MXmBspROkU")
@@ -64,18 +62,14 @@ client.on("messageCreate", (message) => {
 })
 
 
-client.on("guildMemberUpdate", member => {
-if (oldRoles.size < newRoles.size) {
-    client.on('guildMemberUpdate', oldMember, newMember => {
-        const oldRoles = oldMember.roles.cache;
-        const newRoles = newMember.roles.cache;
-        if (oldRoles.size < newRoles.size) {
-          const Embed = new Discord.MessageEmbed()
-          .setTitle('Roles updated!')
-          .setDescription(`${newMember.user.username}'s roles have been updated!`)
-          .setColor(3426654);
-          newMember.guild.channels.cache.find(ch => ch.name.toLowerCase() === 'logs').send(Embed);
-        }
-    })
-}
-});
+client.on('guildMemberUpdate', oldMember, newMember => {
+    const oldRoles = oldMember.roles.cache;
+    const newRoles = newMember.roles.cache;
+    if (oldRoles.size < newRoles.size) {
+        const Embed = new Discord.MessageEmbed()
+        .setTitle('Roles updated!')
+        .setDescription(`${newMember.user.username}'s roles have been updated!`)
+        .setColor(3426654);
+        newMember.guild.channels.cache.find(ch => ch.name.toLowerCase() === 'logs').send(Embed);
+    }
+})
