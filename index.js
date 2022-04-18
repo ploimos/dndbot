faaaaf2c3b65db0e6c7f928f1ffa0848e9714e4c
ploimos <sinnoh98@gmail.com>
2022-04-18 16:20:21 +0200

    4

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ const client = new Discord.Client(
     {intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"]}
 )
 
-client.login(process.env.token)
-//client.login("OTY1MjYyOTEwNTc2Mjk1OTM2.YlwpIw.3g4joeLLpp_ykDY08MXmBspROkU")
+//client.login(process.env.token)
+client.login("OTY1MjYyOTEwNTc2Mjk1OTM2.YlwpIw.3g4joeLLpp_ykDY08MXmBspROkU")
 client.on("ready", () => {
     console.log("Sono Online.")
 })
 
 client.on("messageCreate", (message) => {
-    if (message.member.roles.cache.has("965547318009016330")) {
-        message.reply("You are allowed to use this command.");
-    } else {
-        message.channel.send("You are not allowed to use this command.");
+    if (message.member.roles.cache.has("965547318009016330") && message.author.bot == false) {
+        message.channel.send("You are allowed to use this command.");
     }
 })
 
