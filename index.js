fc397f3571d4585bc7fb8533b179766c85b7def4
ploimos <sinnoh98@gmail.com>
2022-04-18 17:01:51 +0200

    a

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const client = new Discord.Client(
     {intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"]}
 )
 
-client.login(process.env.token)
-//client.login("OTY1MjYyOTEwNTc2Mjk1OTM2.YlwpIw.3g4joeLLpp_ykDY08MXmBspROkU")
+//client.login(process.env.token)
+client.login("OTY1MjYyOTEwNTc2Mjk1OTM2.YlwpIw.3g4joeLLpp_ykDY08MXmBspROkU")
 client.on("ready", () => {
     console.log("Sono Online.")
 })
