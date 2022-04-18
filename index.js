90e444c96b3721f7ec7d4542f964b739532d696b
ploimos <sinnoh98@gmail.com>
2022-04-18 16:58:14 +0200

    a

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const client = new Discord.Client(
     {intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"]}
 )
 
-//client.login(process.env.token)
-client.login("OTY1MjYyOTEwNTc2Mjk1OTM2.YlwpIw.3g4joeLLpp_ykDY08MXmBspROkU")
+client.login(process.env.token)
+//client.login("OTY1MjYyOTEwNTc2Mjk1OTM2.YlwpIw.3g4joeLLpp_ykDY08MXmBspROkU")
 client.on("ready", () => {
     console.log("Sono Online.")
 })
 
 client.on("messageCreate", (message) => {
     if (message.content == "Ciao") {
-        message.channel.send("bello! Saluta!! Tutti");
+        message.channel.send("Salutamelo");
     }
 })
 
