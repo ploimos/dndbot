45048b1b26c746ad425735e5ad5a1a066dea1ec2
ploimos <sinnoh98@gmail.com>
2022-04-18 12:58:30 +0200

    ploimos/dndbot

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const client = new Discord.Client(
 )
 
 client.login(process.env.token)
-
+//client.login("OTY1MjYyOTEwNTc2Mjk1OTM2.YlwpIw.3g4joeLLpp_ykDY08MXmBspROkU")
 client.on("ready", () => {
     console.log("Sono Online.")
 })
 
 client.on("messageCreate", (message) => {
-    if(message.content == ""){
-        
+    if (message.member.roles.cache.has("965547318009016330")) {
+        message.reply("You are allowed to use this command.");
+    } else {
+        message.channel.send("You are not allowed to use this command.");
     }
 })
 
