ca894a0703dddac5a3b28113e20703c32f76e2dd
ploimos <sinnoh98@gmail.com>
2022-04-19 18:09:28 +0200

    db43

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ client.on("messageCreate", (message) => {
                                 ms: num})
                                 console.log("non c'era")
                             /*} else {*/
-                                console.log(name)
+                                console.log("Il tag è"+name)
                                 database.collection("Land").updateOne({id: name}, {$set: {ms: 1}})
                                 console.log("c'era")
                             /*}*/
