fa3fce0324562df0573de15092e77234696e7f9a
ploimos <sinnoh98@gmail.com>
2022-04-19 15:58:14 +0200

    db27

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ client.on("messageCreate", (message) => {
                         " milestone"+s+" a " + message.content.split(" ")[1] + ".");
                         MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, function (err, db){
                             var database = db.db("DbDnD");
-                            var num = message.content.split(" ").slice(-1);
+                            var num = number(message.content.split(" ").slice(-1));
                             var name = message.content.split(" ")[1];
                             database.collection("Land").insertOne({id: name,
                             ms: num});
