68e4489e95db16ad94ba5b6a875c408401882761
ploimos <sinnoh98@gmail.com>
2022-04-19 14:31:43 +0200

    finnan3

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var database;
 var url = "mongodb+srv://botperdnd:cCgYya6YDwnGDH9h@cluster0.kfhj7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 var db = MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, function (err, db){
     var database = db.db("DbDnD");
-    database.collection("Land").insertOne({id: 2333, nome: "Finnan", ms: 22, level: 8, money: 400});
+    database.collection("Land").deleteOne({id: 2333, nome: "Finnan", ms: 22, level: 8, money: 400});
 })
 
 client.on("ready", () => {
