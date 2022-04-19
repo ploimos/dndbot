df0efe216475087a39a744fa6f6004d38136c4cc
ploimos <sinnoh98@gmail.com>
2022-04-19 18:23:37 +0200

    db52

--- index.js
+++ index.js
@@ -49,14 +49,13 @@ client.on("messageCreate", (message) => {
                         MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, function (err, db){
                             var database = db.db("DbDnD");
                             const res = database.collection("Land").findOne({id:name});
-                            /*if (res==""){
+                            if (!res){
                                 database.collection("Land").insertOne({id: name, ms: num})
                                 console.log("non c'era")
                             } else {
                                 database.collection("Land").updateOne({id: name}, {$inc: {ms: num}})
                                 console.log("c'era")
-                            }*/
-                            console.log("Il res è"+res)
+                            }
                         })
                     }
                 }
