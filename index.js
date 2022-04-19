83acd183ccc7ae4e8ecb511529f9663450bedf2b
ploimos <sinnoh98@gmail.com>
2022-04-19 15:08:45 +0200

    db14

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ client.login("OTY1MjYyOTEwNTc2Mjk1OTM2.YlwpIw.3g4joeLLpp_ykDY08MXmBspROkU")
 //var MongoClient = require("mongodb").MongoClient;
 var database;
 var url = "mongodb+srv://botperdnd:"+pass+"@cluster0.kfhj7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-var db = MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, function (err, db){
     var database = db.db("DbDnD");
-    database.collection("Land").insertOne({id: 2332, nome: "Finnan", ms: 22, level: 8, money: 400});
-
+    database.collection("Land").insertOne({id: 2334, nome: "Finnan", ms: 22, level: 8, money: 400});
+})
 
 client.on("ready", () => {
     console.log("ONLINE");
