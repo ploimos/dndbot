8a7c48a1153be4b2a71b4e68a3ad73e77a596c86
ploimos <sinnoh98@gmail.com>
2022-04-19 18:53:09 +0200

    sad

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ client.on("ready", () => {
 client.on("messageCreate", (message) => {
     if(message.author.bot == false && message.channel == canale) {
         if (message.content.split(" ")[0] == "!givems"){
-            if (message.member.roles.cache.has(ruolo)) {
+            if (message.member.roles.cache.has(ruolo)){
                 if (message.content.split(" ")[1].length>1){
                     if(message.content.split(" ").slice(-1)[0]>3 ||
                     message.content.split(" ").slice(-1)[0]<=0 ||
