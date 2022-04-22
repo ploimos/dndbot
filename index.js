6d60dafd86090eeb914377fde336686e78ed137a
ploimos <sinnoh98@gmail.com>
2022-04-22 18:52:19 +0200

    sad

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ client.on("messageCreate", (message) => {
                                 console.log(oldname)
                                 database.collection(col1).updateOne({id: tag}, {$set: {id: tag, nome: name, mo: num, ms: 0}})
                                 //message.reply("Il personaggio che si chiama '"+name+"' ha sovrascritto il vecchio personaggio.");
-                                message.reply("Il personaggio che si chiama '"+name+"' ha sovrascritto '"+oldname+"'.");
+                                message.reply("Il personaggio che si chiama '"+name+"' ha sovrascritto '"+string(oldname)+"'.");
                             }
                         })
 
