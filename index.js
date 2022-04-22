5bb768bd77499d97321c7412f7fe28ec6f601b7d
ploimos <sinnoh98@gmail.com>
2022-04-22 19:21:39 +0200

    sad

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ client.on("messageCreate", (message) => {
                                 database.collection(col1).insertOne({id: tag, nome: name, mo: num, ms: 0})
                             } else {
                                 var oldname;
-                                database.collection(col1).find({id: tag}).toArray(function onam(err,res){
+                                database.collection(col1).find({id: tag}).toArray(function onam(res){
                                     oldname = res[0].nome
                                     return oldname
                                 })
