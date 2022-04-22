5563d646b2bbff5021866629e098fb72a78a3f31
ploimos <sinnoh98@gmail.com>
2022-04-22 18:34:22 +0200

    asd

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ client.on("messageCreate", (message) => {
                             } else {
                                 var oldname;
                                 database.collection(col1).find({id: tag}).toArray(function(err,res){
-                                    console.log(res.name)
+                                    console.log(res)
                                 })
                                 database.collection(col1).updateOne({id: tag}, {$set: {id: tag, nome: name, mo: num, ms: 0}})
                                 //message.reply("Il personaggio che si chiama '"+name+"' ha sovrascritto il vecchio personaggio.");
