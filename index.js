5ef20e128056564cea0c6ac5255f120d7fd9f4f5
ploimos <sinnoh98@gmail.com>
2022-04-22 18:45:02 +0200

    asd

--- index.js
+++ index.js
@@ -145,8 +145,8 @@ client.on("messageCreate", (message) => {
                                 database.collection(col1).insertOne({id: tag, nome: name, mo: num, ms: 0})
                             } else {
                                 database.collection(col1).find({id: tag}).toArray(function(err,res){
-                                    var oldname = res[0].nome
-                                    console.log(oldname)
+                                    res[0].nome
+                                    console.log(res[0].nome)
                                 })
                                 console.log(oldname)
                                 database.collection(col1).updateOne({id: tag}, {$set: {id: tag, nome: name, mo: num, ms: 0}})
