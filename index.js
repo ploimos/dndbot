78bc73117da5fa6cdbfd3c670524af75a7004d6d
ploimos <sinnoh98@gmail.com>
2022-04-22 17:43:15 +0200

    sad

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ client.on("messageCreate", (message) => {
                             if (!database.collection(col1).find({id: tag})) {
                                 database.collection(col1).insertOne({id: tag, nome: name, mo: num, ms: 0})
                             } else {
-                                var oldname = String(database.collection(col1).findOne({id: tag}))
+                                var oldname = database.collection(col1).findOne({id: tag}).then()
                                 console.log("Io leggo:"+oldname)
                                 database.collection(col1).updateOne({id: tag}, {$set: {id: tag, nome: name, mo: num, ms: 0}})
                                 //message.reply("Il personaggio che si chiama '"+name+"' ha sovrascritto il vecchio personaggio.");
