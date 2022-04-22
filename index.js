37d12ebb4ea90afe5403cdcc8fb3cfed7e40b4bb
ploimos <sinnoh98@gmail.com>
2022-04-22 19:21:13 +0200

    sad

--- index.js
+++ index.js
@@ -145,14 +145,14 @@ client.on("messageCreate", (message) => {
                                 database.collection(col1).insertOne({id: tag, nome: name, mo: num, ms: 0})
                             } else {
                                 var oldname;
-                                database.collection(col1).find({id: tag}).toArray(function(err,res){
+                                database.collection(col1).find({id: tag}).toArray(function onam(err,res){
                                     oldname = res[0].nome
                                     return oldname
                                 })
-                                console.log("fuoricoll"+oldname)
+                                console.log("fuoricoll"+onam(oldname))
                                 database.collection(col1).updateOne({id: tag}, {$set: {id: tag, nome: name, mo: num, ms: 0}})
                                 //message.reply("Il personaggio che si chiama '"+name+"' ha sovrascritto il vecchio personaggio.");
-                                message.reply("Il personaggio che si chiama '"+name+"' ha sovrascritto '"+oldname+"'.");
+                                message.reply("Il personaggio che si chiama '"+name+"' ha sovrascritto '"+onam(oldname)+"'.");
                             }
                         })
 
